50111cb7508c0496421d72067cd13aa0f446f217
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-02-25 00:41:34 +0100

    add welcome animation in welcome_screen.dart

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_button/custom/anime_press_button.dart';
@@ -17,72 +18,76 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       backgroundColor: Colors.blueGrey.shade300,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 65,
-                  height: 65,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('images/logo.png'),
-                      fit: BoxFit.cover,
-                    ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/logo.png'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Text(
-                  'CHAT',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 45.0,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: 'BungeeShade'),
+              ),
+              ColorizeAnimatedTextKit(
+                totalRepeatCount: 1,
+                speed: Duration(milliseconds: 300),
+                pause: Duration(milliseconds: 300),
+                text: ['YOURS', 'PRIVATE', 'CHAT'],
+                textStyle: TextStyle(fontSize: 50.0, fontFamily: 'BungeeShade'),
+                colors: [
+                  Colors.black,
+                  Colors.black,
+                  Colors.blue,
+                  Colors.yellow,
+                  Colors.red,
+                ],
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: AnimePressButton(
+                  duration: Duration(milliseconds: 100),
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.red,
+                  onTap: () {},
+                  wGradient: true,
+                  gradientColors: [
+                    Colors.pink,
+                    Colors.purpleAccent,
+                  ],
+                  title: "Log In",
+                  titleColor: Colors.black,
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: AnimePressButton(
-                duration: Duration(milliseconds: 100),
-                borderRadius: BorderRadius.circular(100),
-                color: Colors.red,
-                onTap: () {},
-                wGradient: true,
-                gradientColors: [
-                  Colors.pink,
-                  Colors.purpleAccent,
-                ],
-                title: "Log In",
-                titleColor: Colors.black,
-                fontFamily: 'SourceSansPro',
-                fontWeight: FontWeight.bold,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: AnimePressButton(
-                duration: Duration(milliseconds: 100),
-                borderRadius: BorderRadius.circular(100),
-                color: Colors.red,
-                onTap: () {},
-                wGradient: true,
-                gradientColors: [
-                  Colors.deepPurpleAccent,
-                  Colors.teal,
-                ],
-                title: "Register",
-                titleColor: Colors.black,
-                fontFamily: 'SourceSansPro',
-                fontWeight: FontWeight.bold,
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: AnimePressButton(
+                  duration: Duration(milliseconds: 100),
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.red,
+                  onTap: () {},
+                  wGradient: true,
+                  gradientColors: [
+                    Colors.deepPurpleAccent,
+                    Colors.teal,
+                  ],
+                  title: "Register",
+                  titleColor: Colors.black,
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
