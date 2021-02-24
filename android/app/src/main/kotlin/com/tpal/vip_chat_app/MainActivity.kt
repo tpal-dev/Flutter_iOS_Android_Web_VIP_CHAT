dca17796c902a73e231479a0c5a2dc0c5e336689
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-02-24 18:01:42 +0100

    Create a Skeleton

---
+++ android/app/src/main/kotlin/com/tpal/vip_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tpal.vip_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
